247425d1967780761958c274b7de5a2d9a821c75
ErickDC <erickdiazc717@gmail.com>
2022-12-29 17:43:27 -0500

    minor fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+); 
